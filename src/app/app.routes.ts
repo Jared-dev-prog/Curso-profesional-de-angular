cd2625af4dc6d3d5c55ec7c5e2dd0bccf931e261
Jared Alexander Trejo Larios <jared.trejo@axity.com>
2023-12-01 12:48:50 -0600

    Add children routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,19 @@
 import { Routes } from '@angular/router';
 import { PipesComponent } from './components/pipes/pipes.component';
 import { EstructuralComponent } from './components/estructural/estructural.component';
+import { CompraComponent } from './components/compra/compra.component';
+import { NuevaComponent } from './components/compra/nueva/nueva.component';
+import { HistorialComponent } from './components/compra/historial/historial.component';
 
 export const routes: Routes = [
   { path: 'pipes', component: PipesComponent },
   { path: 'estructural', component: EstructuralComponent },
+  {
+    path: 'compra',
+    component: CompraComponent,
+    children: [
+      { path: 'nueva', component: NuevaComponent },
+      { path: 'historial', component: HistorialComponent },
+    ],
+  },
 ];
---
+++ src/app/components/compra/nueva/nueva.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-nueva',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nueva.component.html',
+  styleUrl: './nueva.component.scss'
+})
+export class NuevaComponent {
+
+}
